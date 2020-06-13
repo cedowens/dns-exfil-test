8ebe8e1f7dcd9c9b858994624a6fcc6d5233a85d
El Bonzo <el-b-bonzo@gmail.com>
2020-06-13 12:27:15 -0500

    Additional test

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -1,6 +1,7 @@
 package cmd_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/dns-tun/cmd"
@@ -16,7 +17,7 @@ const (
 	listenPortEnvVar = "LISTEN_PORT_ENV_VAR"
 )
 
-// TestGetDefaultValue tests we receive the default value when the env var isn't set
+// TestGetDefaultValue tests we receive the default value when the env var isn't set.
 func TestGetDefaultValue(t *testing.T) {
 
 	table := []struct {
@@ -35,5 +36,29 @@ func TestGetDefaultValue(t *testing.T) {
 			checkers.Equals(t, tc.got, tc.want)
 		})
 
+	}
+}
+
+// TestDetEnvVarValue tests that we receive the env var defined value.
+func TestDetEnvVarValue(t *testing.T) {
+	os.Setenv(listenPortEnvVar, "12345")
+	os.Setenv(listenAddrEnvVar, "1.1.1.1")
+
+	defer os.Unsetenv(listenPortEnvVar)
+	defer os.Unsetenv(listenAddrEnvVar)
+	table := []struct {
+		name string
+		got  string
+		want string
+	}{
+		{"envPortValue", cmd.GetEnvValue(listenPortEnvVar, defaultListenPort), "12345"},
+		{"envListAddr", cmd.GetEnvValue(listenAddrEnvVar, defaultListenAddr), "1.1.1.1"},
+	}
+
+	for _, tc := range table {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			checkers.Equals(t, tc.got, tc.want)
+		})
 	}
 }
